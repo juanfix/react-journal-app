6e50f3c26aa073881158174437ec09727ca2a62d
Juan Jose Ceballos Navia <juanfix@gmail.com>
2023-04-10 21:15:39 -0500

    .env fixed

--- src/helpers/getEnvironments.js
+++ src/helpers/getEnvironments.js
@@ -1,7 +1,12 @@
+/**
+ * This function returns the environment variables using the import.meta.env object in JavaScript.
+ * @returns The function `getEnvironments` returns an object that contains all the properties of the
+ * `import.meta.env` object.
+ */
 export const getEnvironments = () => {
-  import.meta.env;
+  const env = import.meta.env;
 
   return {
-    ...import.meta.env,
+    ...env,
   };
 };
